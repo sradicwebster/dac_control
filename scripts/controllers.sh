d0cae4d3775fc6547c5dba1869b6964e8e71ae81
Stefan <hn19405@bristol.ac.uk>
2023-07-20 12:42:05 +0100

    misc changes

--- scripts/controllers.sh
+++ scripts/controllers.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 wandb online
-python system_simulation.py +T=48 +naming='[controller]'
-python system_simulation.py +T=48 +wind_max_only=True +naming='[controller,wind_max_only]'
-python system_simulation.py +T=48 controller=cem +dynamics_model=constant_wind +naming='[controller,dynamics_model]'
-python system_simulation.py +T=48 controller=cem +dynamics_model=known_wind +naming='[controller,dynamics_model]'
+python system_simulation.py +naming='[controller]'
+python system_simulation.py +wind_max_only=True +naming='[controller,wind_max_only]'
+python system_simulation.py controller=cem +dynamics_model=constant_wind +naming='[controller,dynamics_model]'
+python system_simulation.py +T=8750 controller=cem +dynamics_model=known_wind +naming='[controller,dynamics_model]'
 wandb offline
